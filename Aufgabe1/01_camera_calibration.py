3f1add7c6241ff7a16aee51d8345a6f7e363683e
Alex <alexkasmale@web.de>
2018-05-04 20:01:42 +0200

    aufgabe 2 init

--- Aufgabe1/01_camera_calibration.py
+++ Aufgabe1/01_camera_calibration.py
@@ -8,8 +8,8 @@ import glob
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6 * 7, 3), np.float32)
-objp[:, :2] = np.mgrid[0:7, 0:6].T.reshape(-1, 2)
+objp = np.zeros((7 * 7, 3), np.float32)
+objp[:, :2] = np.mgrid[0:7, 0:7].T.reshape(-1, 2)
 
 # Arrays to store object points and image points from all the images.
 objpoints = []  # 3d point in real world space
@@ -17,17 +17,17 @@ imgpoints = []  # 2d points in image plane.
 images = glob.glob('images/*.jpg')
 
 # https://longervision.github.io/2017/03/19/opencv-internal-calibration-chessboard/
-# cap = cv2.VideoCapture(0)
-cap = cv2.VideoCapture("http://192.168.178.21:4747/mjpegfeed")
+cap = cv2.VideoCapture(0)
+# cap = cv2.VideoCapture("http://192.168.178.21:4747/mjpegfeed")
 # cap = cv2.VideoCapture("http://141.64.166.253:4747/mjpegfeed")
 found = 0
-while found < 1:  # Here, 10 can be changed to whatever number you like to choose
+while found < 10:  # Here, 10 can be changed to whatever number you like to choose
     ret, img = cap.read()  # Capture frame-by-frame
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # cv2.imshow("calibration", img)
 
     # Find the chess board corners
-    ret, corners = cv2.findChessboardCorners(gray, (7, 6), None)
+    ret, corners = cv2.findChessboardCorners(gray, (7, 7), None)
 
     # If found, add object points, image points (after refining them)
     if ret == True:
@@ -37,7 +37,7 @@ while found < 1:  # Here, 10 can be changed to whatever number you like to choos
         imgpoints.append(corners2)
 
         # Draw and display the corners
-        img = cv2.drawChessboardCorners(img, (7, 6), corners2, ret)
+        img = cv2.drawChessboardCorners(img, (7, 7), corners2, ret)
         cv2.imshow('img', img)
         cv2.waitKey(500)
         cv2.imwrite('img.png', img)
