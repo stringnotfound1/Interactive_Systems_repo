1bcd0ad83afce0b0e2a6bac8ba0c37b03709a6d3
Alex <alexkasmale@web.de>
2018-05-05 12:37:31 +0200

    Aufgabe 2.1

--- Aufgabe2/02_features.py
+++ Aufgabe2/02_features.py
@@ -1,15 +1,28 @@
 import cv2
 
-cap = cv2.VideoCapture(0)
+# cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture("http://192.168.178.21:4747/mjpegfeed")
+sift = cv2.xfeatures2d.SIFT_create()
 cv2.namedWindow('Interactive Systems: Towards AR Tracking')
+
 while True:
+    ch = cv2.waitKey(1) & 0xFF
+    if ch == ord('q'):
+        break
+    # Capture frame-by-frame
+    ret, frame = cap.read()
 
-    # 1. read each frame from the camera (if necessary resize the image)
-    #    and extract the SIFT features using OpenCV methods
-    #    Note: use the gray image - so you need to convert the image
-    # 2. draw the keypoints using cv2.drawKeypoints
-    #    There are several flags for visualization - e.g. DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    kp = sift.detect(gray, None)
+    # https://github.com/opencv/opencv/issues/6487
+    img = cv2.drawKeypoints(gray, kp, None, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    cv2.imshow("Sift", img)
 
-    # close the window and application by pressing a key
+cv2.destroyAllWindows()
 
-    # YOUR CODE HERE
+
+
+
+
+
