bdc39fdfa90fc1066c98ea9c96527f4397740860
Alex <alexkasmale@gmail.com>
2018-05-23 23:40:59 +0200

    eigen WIP

--- Aufgabe2/02_eigen.py
+++ Aufgabe2/02_eigen.py
@@ -44,26 +44,38 @@ ktrans = k.transpose()
 Gx = cv2.filter2D(img, -1, k)
 Gy = cv2.filter2D(img, -1, ktrans)
 
-#print "dx / dy", Gx, Gy
-
 # this is the 2x2 matrix we need to evaluate
 # the Harris corners
 eigMat = np.zeros((2, 2), np.float32)
 
 # compute values for matrix eigMat and fill matrix with
 # necessary values
-eigMat = cv2.cornerHarris(img, 2, 3, 0.04)
+# eigMat = cv2.cornerHarris(img, 2, 3, 0.04)
+
+Ixx = Gx ** 2
+Iyy = Gy ** 2
+Ixy = Gx * Gy
+
+eigMat[0, 0] = Ixx.sum()
+eigMat[1, 0] = Ixy.sum()
+eigMat[0, 1] = Ixy.sum()
+eigMat[1, 0] = Iyy.sum()
+
+# print(eigMat)
 # YOUR CODE HERE
 
 # compute eigenvectors and eigenvalues using the numpy
 # linear algebra package
-w = np.linalg.eig(eigMat)
-v = np.linalg.eigvals(eigMat)
+w, v = np.linalg.eig(eigMat)
+# v = np.linalg.eigvals(eigMat)
 # YOUR CODE HERE
 
 # out and show the image
 print("matrix:", eigMat, '\n')
-print("eigvalues", w, "eigenvecv", v)
+print("eigvalues", w)
+print( "eigenvecv", v)
+# print("eigvalues", w[0])
+# print( "eigenvecv", v[:, 0])
 scaling_factor = 100
 img = cv2.resize(img, None, fx=scaling_factor, fy=scaling_factor, interpolation=cv2.INTER_AREA)
 cv2.imshow('img', img)
