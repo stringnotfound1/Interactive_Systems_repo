67655a821b87df6fe0b6071ddfa9c4923a05f26b
Alex <alexkasmale@gmail.com>
2018-05-30 14:47:29 +0200

    Aufgabe 3.1 WIP

--- Aufgabe3/02_features.py
+++ Aufgabe3/02_features.py
@@ -5,10 +5,10 @@ from Aufgabe3.ImageStitcher import *
 cap = cv2.VideoCapture("http://192.168.178.21:4747/mjpegfeed")
 # cap = cv2.VideoCapture("http://141.64.175.37:4747/mjpegfeed")
 sift = cv2.xfeatures2d.SIFT_create()
-cv2.namedWindow('Interactive Systems: Towards AR Tracking')
 
 img_marker = cv2.imread('images/marker.jpg')
 img_marker_gray = cv2.cvtColor(img_marker, cv2.COLOR_BGR2GRAY)
+(h1, w1) = img_marker.shape[:2]
 
 imageStitcher = ImageStitcher([img_marker])
 
@@ -20,16 +20,18 @@ while True:
         break
     # Capture frame-by-frame
     ret, frame = cap.read()
+    # frame = cv2.resize(frame, (h1, w1))
 
     # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     # keypoint matching
     kp_frame, dsc_frame = sift.detectAndCompute(gray, None)
     M = imageStitcher.match_keypoints(kp_frame, kp_marker, dsc_frame, dsc_marker)
-    if M is not None:
+    H, status, matches = M
+    if status is not None:
         print(M)
-        H, status, matches = M
         matchedImage = imageStitcher.draw_matches(img_marker, frame, kp_frame, kp_marker, matches, status)
         cv2.imshow("Sift", matchedImage)
+    # M = None ????
 
 cv2.destroyAllWindows()
