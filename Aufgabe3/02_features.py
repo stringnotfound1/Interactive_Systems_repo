fddee94d47172dbcb3654ea0f4e52acd21d1159b
Alex <alexkasmale@gmail.com>
2018-06-01 15:17:34 +0200

    Aufgabe 3.1 done

--- Aufgabe3/02_features.py
+++ Aufgabe3/02_features.py
@@ -10,8 +10,6 @@ img_marker = cv2.imread('images/marker.jpg')
 img_marker_gray = cv2.cvtColor(img_marker, cv2.COLOR_BGR2GRAY)
 (h1, w1) = img_marker.shape[:2]
 
-imageStitcher = ImageStitcher([img_marker])
-
 kp_marker, dsc_marker = sift.detectAndCompute(img_marker_gray, None)
 
 while True:
@@ -25,18 +23,31 @@ while True:
     # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     # keypoint matching
+    imageStitcher = ImageStitcher([img_marker, frame])
     kp_frame, dsc_frame = sift.detectAndCompute(gray, None)
     if kp_frame is not None and dsc_frame is not None:
-        M = imageStitcher.match_keypoints(kp_frame, kp_marker, dsc_frame, dsc_marker)
+        # M = imageStitcher.match_keypoints(kp_frame, kp_marker, dsc_frame, dsc_marker)
+        M = imageStitcher.match_keypoints(kp_marker, kp_frame, dsc_marker, dsc_frame)
         print(M)
         if M is not None:
             H, status, matches = M
             if status is not None and matches is not None and H is not None:
-                newImage = cv2.warpPerspective(img_marker, H, (img_marker.shape[1] + frame.shape[1], img_marker.shape[0] + frame.shape[0]))
-                newImage[0:frame.shape[0], 0:frame.shape[1]] = frame
-                matchedImage = imageStitcher.draw_matches(img_marker, frame, kp_frame, kp_marker, matches, status)
+                # newImage = cv2.warpPerspective(img_marker, H, (img_marker.shape[1] + frame.shape[1], img_marker.shape[0] + frame.shape[0]))
+                # newImage[0:frame.shape[0], 0:frame.shape[1]] = frame
+                matchedImage = imageStitcher.draw_matches(img_marker, frame, kp_marker, kp_frame, matches, status)
                 cv2.imshow("Sift", matchedImage)
         # cv2.imshow("Sift", frame)
     # M = None ????
+
+#
+# kp_frame, dsc_frame = sift.detectAndCompute(img_marker_gray, None)
+# M = imageStitcher.match_keypoints(kp_frame, kp_marker, dsc_frame, dsc_marker)
+# print(M)
+# if M is not None:
+#     H, status, matches = M
+#     if status is not None and matches is not None and H is not None:
+#         matchedImage = imageStitcher.draw_matches(img_marker, img_marker, kp_frame, kp_marker, matches, status)
+#         cv2.imshow("Test", matchedImage)
+# cv2.waitKey()
 
 cv2.destroyAllWindows()
